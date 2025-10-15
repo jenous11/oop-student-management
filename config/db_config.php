7d0499791f9e325c3578c789307f3d064bb68826
jenous11 <jenousdongol11@gmail.com>
2025-10-15 20:32:43 +0545

    manged some cofigs of db connection

--- config/db_config.php
+++ config/db_config.php
@@ -1,7 +1,23 @@
 <?php
-return [
-    'host'=>'localhost',
-    'dbname'=>'student_DB',
-    'username'=>'root',
-    'password'=>'chelse@11'
-];
+// here, the config and connect are at same place which is apparently not good for bigger projects
+class Dbh
+
+{
+    private $host="localhost";
+    private $dbname="student_DB";
+    private $dbusername="root";
+    private $password="chelse@11";
+
+    protected function connect(){
+        try
+        {
+            $pdo = new PDO('mysqli:host='.$this->host.';dbname= '.$this->dbname.';dbusername='.$this->dbusername.';password '.$this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "connected successfully";        
+        }
+        catch(PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
